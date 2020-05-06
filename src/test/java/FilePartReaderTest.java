47b67ff6bb15421084563cd7f2316d0c292d7d2b
Tóth Máté <tothmate911@gmail.com>
2020-05-06 15:13:18 +0200

    start making tests

--- src/test/java/FilePartReaderTest.java
+++ src/test/java/FilePartReaderTest.java
@@ -1,5 +1,14 @@
+import org.junit.Test;
+
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class FilePartReaderTest {
+public class FilePartReaderTest {
 
+    @Test
+    public void AreInstanceVariablesInvalid() {
+        FilePartReader fpr = new FilePartReader();
+        assertThrows(IOException.class, fpr::read);
+    }
 }
